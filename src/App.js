4f3c6598725f9d788f0eee77d5da194e5b0b62e3
sparshdang <dang.sparsh01@gmail.com>
2023-07-27 19:59:26 +0530

    Added saving notes to local storage

--- src/App.js
+++ src/App.js
@@ -1,59 +1,64 @@
-import { useState } from "react";
-import ReactDom from 'react-dom';
+import { useState, useEffect } from "react";
+import ReactDom from "react-dom";
 import Notes from "./components/Notes/Notes";
 import AddNote from "./components/AddNote/AddNote";
 
 import AddNoteButton from "./components/AddNote/AddNoteButton";
 
 function App() {
-  const [addNote, setAddNote] =  useState(false)
-  const [notes, setNotes] =useState([]);
+  const [addNote, setAddNote] = useState(false);
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    const storedNotes = localStorage.getItem("notes");
+    if (storedNotes){
+      setNotes(JSON.parse(storedNotes));
+    }
+  }, []);
 
   const AddNoteHandler = (text) => {
-    setNotes(
-      (prev) => {
-        let id=0;
-        if (prev.length > 0){
-          id = prev[prev.length-1].id + 1
-        }
-
-        const note = {
-          text:text,
-          id:id
-        }
-
-        return [note, ...prev];
+    setNotes((prev) => {
+      let id = 0;
+      if (prev.length > 0) {
+        id = prev[0].id + 1;
       }
-    )
-  }
 
-  const closeOverlay = () =>{
+      const note = {
+        text: text,
+        id: id,
+      };
+
+      const newNotes = [note, ...prev];
+      localStorage.setItem("notes", JSON.stringify(newNotes));
+      return newNotes;
+    });
+  };
+
+  const closeOverlay = () => {
     setAddNote(false);
-  }
+  };
 
-  const openOverlayHandler = () =>{
+  const openOverlayHandler = () => {
     setAddNote(true);
-  }
+  };
 
-  const deleteNote  = (id) =>{
-    setNotes(
-      (prev) => {
+  const deleteNote = (id) => {
+    setNotes((prev) => {
+      const newNotes = prev.filter((note) => note.id !== id);
+      localStorage.setItem("notes", JSON.stringify(newNotes));
 
-        return prev.filter( (note) => note.id !== id )
-      }
-    )
-  }
+      return newNotes;
+    });
+  };
   return (
     <div>
-      <Notes notes={notes} onDelete={deleteNote}/>
+      <Notes notes={notes} onDelete={deleteNote} />
 
       {ReactDom.createPortal(
-        addNote && <AddNote onAdd={AddNoteHandler} close={closeOverlay}/>,
-        document.getElementById('overlay')
-
+        addNote && <AddNote onAdd={AddNoteHandler} close={closeOverlay} />,
+        document.getElementById("overlay")
       )}
-      {!addNote && <AddNoteButton onOpenAdd={openOverlayHandler}/>}
-
+      {!addNote && <AddNoteButton onOpenAdd={openOverlayHandler} />}
     </div>
   );
 }
--- src/components/Notes/Notes.jsx
+++ src/components/Notes/Notes.jsx
@@ -1,25 +1,21 @@
-import React from 'react'
-import Note from './Note'
+import React from "react";
+import Note from "./Note";
 
-import styles from './Notes.module.css'
-
-
-
-
+import styles from "./Notes.module.css";
 
 export default function Notes(props) {
   return (
     <>
       <h1 className={styles.heading}>Notes</h1>
-      <div className={styles['notes-container']}>
-      {
-        props.notes.map(
-            (note)=>{
-                return <Note key={note.id} note={note} onDelete={props.onDelete}/>;
-            }
-        )
-      }
-      </div>
+      {props.notes.length === 0 ? (
+        <h2 className={styles['empty-notes-message']}>You don't have any notes. Try Adding one!!!</h2>
+      ) : (
+        <div className={styles["notes-container"]}>
+          {props.notes.map((note) => {
+            return <Note key={note.id} note={note} onDelete={props.onDelete} />;
+          })}
+        </div>
+      )}
     </>
-  )
+  );
 }
